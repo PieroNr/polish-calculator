c500c131dcc133536044873ccd2165ea1555eece
Piero <piero.neri@hotmail.fr>
2022-05-05 00:04:36 +0200

    add tests error number of operator

--- script/tests/unit/index.spec.ts
+++ script/tests/unit/index.spec.ts
@@ -11,7 +11,18 @@ test("When array of array of number and array of operator is request, it should 
 })
 
 test("When array of array of number and array of operator is request, it should return the result", function () {
+    const calcul: [Array<number>,Array<Operator>] = [[10,13,41,9,10,4],['*','-','-','/','+']];
+    expect(calculatePolish('',calcul)).toEqual(12);
+})
+
+test("When array of array of number and array of operator is request, it should return the result", function () {
     const calcul: [Array<number>,Array<Operator>] = [[4,2,3],['+','-','+']];
     expect(calculatePolish('',calcul)).toEqual('error : Nombre d\'opérateur incorrect');
 })
+
+test("When array of array of number and array of operator is request, it should return the result", function () {
+    const calcul: [Array<number>,Array<Operator>] = [[4,2,3],['+']];
+    expect(calculatePolish('',calcul)).toEqual('error : Nombre d\'opérateur incorrect');
+})
+
 
