7b6d464b8e2e4aed5d0cc674cc7f220171ed805d
timothee-durand <timothee.durand70@gmail.com>
2022-05-06 23:00:15 +0200

    add parseInput function and tests

--- script/index.ts
+++ script/index.ts
@@ -1,13 +1,87 @@
 
 export type Operator = '+' | '-' | '*' | '/';
 
-export const calculatePolish = (calculStr: string, calculTab: [Array<number>,Array<Operator>]): number | string => {
-    /* const {<number>, <Operator>} = parseInput(calculStr); */
-    const tabNum = calculTab[0];
-    const tabOpe = calculTab[1];
+export type Negate = 'NEGATE'
+
+export type ParseResult = {numbers : Array<number>, operators : Array<Operator>}
+
+export const ERROR_NEGATE = "Negate must be placed right after a number !"
+export const ERROR_UNSPECIFIED = "A calcul must be specified !"
+export const ERROR_INCORRECT_OPERATORS = "Incorrect operator number !"
+
+
+export const isOperator = (string : string) : Operator | false => {
+    return string === "+" || string === "-" || string === "*" || string === "/" ? string : false;
+}
+
+export const isNegate = (string : string) : boolean => {
+    return string === "NEGATE";
+}
+
+export const isFloat = (string : string) : number | false => {
+    const number = parseFloat(string)
+        return !isNaN(number) ? number : false;
+}
+
+
+export const parseInput = (input : string) : ParseResult => {
+    const baseArray : string[] = input.split(' ')
+    const numbers : number[] = []
+    const operators : Operator[] = []
+
+    baseArray.forEach((item, i) => {      
+        if(item === '') {
+            return
+        }
+
+        const number = isFloat(item)
+        if(number) {
+            numbers.push(number)
+            return
+        }
+
+        const operator = isOperator(item)
+        if(operator) {
+            operators.push(operator)
+            return
+        }
+
+        const negate = isNegate(item)
+        if(negate) {
+            const previousIndex = baseArray[i - 1]
+            if (previousIndex && isFloat(previousIndex)) {
+                numbers[numbers.length - 1] = - numbers[numbers.length - 1]
+                return
+            } 
+            throw new Error(ERROR_NEGATE)           
+        }
+    })
+    
+    return {
+        numbers,
+        operators
+    }
+}
+
+export const calculatePolish = (calculStr: string | null = null , calculTab : ParseResult | null = null): number | string | Error => {
+    let tabNum: Array<number>; 
+    let tabOpe: Array<Operator>;
+    if(calculStr){
+        const calculArray: ParseResult = parseInput(calculStr);
+        tabNum = calculArray.numbers;
+        tabOpe = calculArray.operators;
+    } else if(calculTab){
+        tabNum= calculTab.numbers;
+        tabOpe = calculTab.operators;
+    } else {
+        return new Error(ERROR_UNSPECIFIED); 
+    }
+    
+    
+    
 
     if(tabOpe.length != tabNum.length-1){
-        return 'error : Nombre d\'opérateur incorrect'
+        throw new Error(ERROR_INCORRECT_OPERATORS)
     }
 
     tabOpe.forEach(operator => {
--- script/tests/unit/index.spec.ts
+++ script/tests/unit/index.spec.ts
@@ -1,28 +1,70 @@
-import { Operator, calculatePolish } from "../../index";
+import { Operator, calculatePolish, parseInput, ERROR_NEGATE, ParseResult, isFloat, isNegate, isOperator, ERROR_INCORRECT_OPERATORS } from "../../index";
 
 test("When array of array of number and array of operator is request, it should return the result", function () {
-    const calcul: [Array<number>,Array<Operator>] = [[1,2,3],['+','+']];
-    expect(calculatePolish('',calcul)).toEqual(6);
+    const calcul: ParseResult = {numbers: [1,2,3], operators: ['+','+']};
+    expect(calculatePolish('', calcul)).toEqual(6);
 });
 
 test("When array of array of number and array of operator is request, it should return the result", function () {
-    const calcul: [Array<number>,Array<Operator>] = [[4,2,3],['+','-']];
-    expect(calculatePolish('',calcul)).toEqual(3);
+    const calcul: ParseResult = {numbers: [4,2,3], operators: ['+','-']};
+    expect(calculatePolish('', calcul)).toEqual(3);
 })
 
 test("When array of array of number and array of operator is request, it should return the result", function () {
-    const calcul: [Array<number>,Array<Operator>] = [[10,13,41,9,10,4],['*','-','-','/','+']];
-    expect(calculatePolish('',calcul)).toEqual(12);
+    const calcul: ParseResult = {numbers: [10,13,41,9,10,4], operators: ['*','-','-','/','+']};
+    expect(calculatePolish('', calcul)).toEqual(12);
 })
 
 test("When array of array of number and array of operator is request, it should return the result", function () {
-    const calcul: [Array<number>,Array<Operator>] = [[4,2,3],['+','-','+']];
-    expect(calculatePolish('',calcul)).toEqual('error : Nombre d\'opérateur incorrect');
+    const calcul: ParseResult = {numbers: [4,2,3], operators: ['+','-','+']};
+    const triggerError = () => {
+        calculatePolish('', calcul)
+    }
+    expect(triggerError).toThrowError(ERROR_INCORRECT_OPERATORS);
 })
 
 test("When array of array of number and array of operator is request, it should return the result", function () {
-    const calcul: [Array<number>,Array<Operator>] = [[4,2,3],['+']];
-    expect(calculatePolish('',calcul)).toEqual('error : Nombre d\'opérateur incorrect');
+    const calcul:ParseResult = {numbers: [4,2,3], operators: ['+']};
+    expect(calculatePolish('', calcul)).toEqual('error : Nombre d\'opérateur incorrect');
 })
 
+test("When a string is given to parseInput it return a correct ParseResult", function() {
+    const input = "3 4 2 + -";
+    expect(parseInput(input)).toStrictEqual({
+        numbers : [3, 4, 2],
+        operators : ['+', '-']
+    })
+})
 
+test("When a string is given to parseInput with NEGATE it return a correct ParseResult", function() {
+    const input = "78 10 NEGATE 9 / *";
+    expect(parseInput(input)).toStrictEqual({
+        numbers : [78, -10, 9],
+        operators : ['/', '*']
+    })
+
+    const testError = () => {
+        const input2 = "NEGATE 5 2 +";
+        parseInput(input2)
+    }
+    
+    expect(testError).toThrowError(ERROR_NEGATE)
+})
+
+test("When a number is passed to isFloat, it return a number but if not it return false", function() {
+    expect(isFloat('5')).toEqual(5)
+    expect(isFloat('test')).toEqual(false)
+})
+
+test("When NEGATE is passed to isNegate, it return a true but if not it return false", function() {
+    expect(isNegate('NEGATE')).toEqual(true)
+    expect(isNegate('test')).toEqual(false)
+})
+
+test("When an operator is passed to isOperator, it return the operator but if not it return false", function() {
+    expect(isOperator('+')).toEqual('+')
+    expect(isOperator('-')).toEqual('-')
+    expect(isOperator('*')).toEqual('*')
+    expect(isOperator('/')).toEqual('/')
+    expect(isOperator('test')).toEqual(false)
+})
